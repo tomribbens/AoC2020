20c50443094d9edf8965838f92836f42a896ebd6
Tom Ribbens <tom@tomribbens.be>
2020-12-05 12:53:44 +0100

    Solutions for day 5

---
+++ day5b.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+# Day 5, part 1 of Advent of Code
+# https://adventofcode.com/2020/day/1
+
+import sys
+
+boarding_passes = []
+maxseatid = 0
+with open(str(sys.argv[1])) as input_file:
+    for line in input_file:
+        line = line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
+        row = int(line[0:7], 2)
+        column = int(line[7:10], 2)
+        seatid = (row * 8) + column
+        boarding_passes.append({
+            "row": row,
+            "column": column,
+            "seatid": seatid
+ 
+        })
+
+        if seatid > maxseatid:
+            maxseatid = seatid
+
+
+print("Highest Seat ID is: ", maxseatid)
+
+
+missing = -1
+yourseat = 0
+for i in range(1, maxseatid):
+    seat = next((seat for seat in boarding_passes if seat["seatid"] == i), None)
+    if seat == None:
+        if missing == i - 1: continue
+        yourseat = i
+
+print("Your seat is: ", yourseat)
+        
+
